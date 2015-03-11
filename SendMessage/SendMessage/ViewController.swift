445e2345bd807443dee633911573f7020cb202d0
Ron Gebauer <ron@ip-145-144-248-125.workstations.fontys.nl>
2015-03-11 15:46:29 +0100

    actualize

---
+++ SendMessage/SendMessage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SendMessage
+//
+//  Created by Ron Gebauer on 11.03.15.
+//  Copyright (c) 2015 Ron Gebauer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
